4c9645b87db433eb965eb1ca28767fb86280710c
root <root@DESKTOP-H9275MT.localdomain>
2024-02-01 15:53:11 +0000

    0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""UTF-8 Validation"""
+
+
+def validUTF8(data):
+    """
+    data: list of integers
+    Return: True if data is valid UTF-8
+    encoding, else False
+    """
+    byte_count = 0
+
+    for i in data:
+        """Check if the integers is within the valid byte range"""
+        if i < 0 or i > 255:
+            return False
+
+        if byte_count == 0:
+            if i >> 5 == 0b110:
+                byte_count == 1
+            elif i >> 4 == 0b1110:
+                byte_count == 2
+            elif i >> 3 == 0b11110:
+                byte_count == 3
+            elif 1 >> 7 == 0b1:
+                return False
+        else:
+            if i >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    return byte_count == 0
