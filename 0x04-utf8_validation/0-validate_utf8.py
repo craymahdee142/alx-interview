c6a8c9eac691e9a8019b4776691e35348692b0c3
root <root@DESKTOP-H9275MT.localdomain>
2024-02-01 16:31:01 +0000

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,22 +11,31 @@ def validUTF8(data):
     """
     byte_count = 0
 
+    # Mask to check the bits of each byte
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
     for i in data:
         """Check if the integers is within the valid byte range"""
-        if i < 0 or i > 255:
-            return False
+        i = i & 255
 
         if byte_count == 0:
-            if i >> 5 == 0b110 or i >> 5 == 0b1110:
-                byte_count == 1
-            elif i >> 4 == 0b1110:
-                byte_count == 2
-            elif i >> 3 == 0b11110:
-                byte_count == 3
-            elif 1 >> 7 == 0b1:
+            # Determine the number of bytes in the uTF-8
+            mask = 1 << 7
+
+            while i & mask:
+                byte_count =+ 1
+                mask = mask >> 1
+
+            # If it's a single byte character
+            if byte_count == 0:
+                continue
+            # UTF-8 charac can be 1 or 4 long
+            if byte_count == 1 or byte_count > 4:
                 return False
         else:
-            if i >> 6 != 0b10:
+            if not (i & mask1 and not (i & mask2)):
                 return False
-            byte_count -= 1
+        byte_count -= 1
+
     return byte_count == 0
