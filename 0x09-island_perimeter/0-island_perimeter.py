5801b55ca758ab4dcc25133aa9791aae9348a54b
root <root@DESKTOP-H9275MT.localdomain>
2024-03-07 15:11:32 +0000

    0x09. Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module for Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid.
+
+
+    Args:
+        grid (list of list of int): the grid representing the island
+
+    Returns:
+        int: the perim of the island
+    """
+    # Determine the number of rows and columns in the grid
+    rows = len(grid)
+    cols = len(grid[0])
+
+    # Initialize the perim variable to 0
+    perimeter = 0
+
+    # Loop through each cell in the grid
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Check the top edge
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                # Check the bottom edge
+                if i == rows-1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                # Check the left edge
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                # Check the right edge
+                if j == cols-1 or grid[i][j+1] == 0:
+                    perimeter += 1
+
+    return perimeter
